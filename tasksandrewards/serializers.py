dbf1fb0470bb7290e55d23afb1bb621249eca7ef
Ryan Burton <rryanburton@gmail.com>
2017-12-21 13:53:09 -0500

    reformatting

--- tasksandrewards/serializers.py
+++ tasksandrewards/serializers.py
@@ -2,36 +2,44 @@ from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from tasksandrewards.models import Player, Task, Reward, RedeemedReward, CompletedTask
 
+
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ('id', 'name',)
 
+
 class UserSerializer(serializers.ModelSerializer):
-    groups = GroupSerializer(many=True)
+    groups = GroupSerializer(many=True, read_only=True)
+
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'groups',)
+
 
 class PlayerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Player
         fields = ('name', 'score', 'created_at', 'updated_at', 'user', 'id',)
 
+
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Task
         fields = ('name', 'task', 'points', 'created_at', 'updated_at', 'id',)
+
 
 class RewardSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Reward
         fields = ('name', 'reward', 'cost', 'id',)
 
+
 class RedeemedRewardSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = RedeemedReward
         fields = ('reward', 'player', 'created_at', 'id',)
+
 
 class CompletedTaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
--- tasksandrewards/views.py
+++ tasksandrewards/views.py
@@ -1,31 +1,46 @@
 from rest_framework import viewsets
 from django.contrib.auth.models import User, Group
-from tasksandrewards.serializers import UserSerializer, GroupSerializer, PlayerSerializer, TaskSerializer, RewardSerializer, RedeemedRewardSerializer, CompletedTaskSerializer
+from tasksandrewards.serializers import (
+        UserSerializer,
+        GroupSerializer,
+        PlayerSerializer,
+        TaskSerializer,
+        RewardSerializer,
+        RedeemedRewardSerializer,
+        CompletedTaskSerializer
+    )
 from tasksandrewards.models import Player, Task, Reward, RedeemedReward, CompletedTask
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
 
 class PlayerViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
 
+
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
 
 class RewardViewSet(viewsets.ModelViewSet):
     queryset = Reward.objects.all()
     serializer_class = RewardSerializer
 
+
 class RedeemedRewardViewSet(viewsets.ModelViewSet):
     queryset = RedeemedReward.objects.all()
     serializer_class = RedeemedRewardSerializer
+
 
 class CompletedTaskViewSet(viewsets.ModelViewSet):
     queryset = CompletedTask.objects.all()
