1206b91803291ff52370167fe4e82df3eecace40
Ryan Burton <rryanburton@gmail.com>
2017-12-21 12:10:45 -0500

    added initial admin setup

--- tasksandrewards/admin.py
+++ tasksandrewards/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Player, Task, Reward, RedeemedReward, CompletedTask
 
-# Register your models here.
+
+class PlayerAdmin(admin.ModelAdmin):
+    queryset = Player.objects.all()
+    list_display = ('name', 'score', 'created_at', 'updated_at', 'user', 'id',)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    queryset = Task.objects.all()
+    list_display = ('name', 'task', 'points', 'created_at', 'updated_at', 'id',)
+
+
+class RewardAdmin(admin.ModelAdmin):
+    queryset = Reward.objects.all()
+    list_display = ('name', 'reward', 'cost', 'id',)
+
+
+class RedeemedRewardAdmin(admin.ModelAdmin):
+    queryset = RedeemedReward.objects.all()
+    list_display = ('reward', 'player', 'created_at', 'id',)
+
+
+class CompletedTaskAdmin(admin.ModelAdmin):
+    queryset = CompletedTask.objects.all()
+    list_display = ('task', 'player', 'created_at', 'id',)
+
+
+admin.site.register(Player, PlayerAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Reward, RewardAdmin)
+admin.site.register(RedeemedReward, RedeemedRewardAdmin)
+admin.site.register(CompletedTask, CompletedTaskAdmin)
